3da1d40cfc4601222ff1f8e7559197f2c1331066
Muhammed Barghou <muhammedbarghou@gmail.com>
2025-04-03 14:58:38 +0000

    initialize project with Next.js, Tailwind CSS, and ESLint configuration

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import {Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Preap-Now",
+  description: "An Ai-Powered Interview Mock-Up Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.variable}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div>
+            <h1>
+                hello
+            </h1>
+        </div>
+    );
+}
+
+export default Page;
